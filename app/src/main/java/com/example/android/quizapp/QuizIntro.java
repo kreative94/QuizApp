886c15b096398ecbf9908bcf2736866ca03a76c4
Karelle Hofler <karellehofler@gmail.com>
2018-03-27 14:32:03 -0400

    Add Fragment added to WomenHistoryQuiz Activity

--- app/src/main/java/com/example/android/quizapp/QuizIntro.java
+++ app/src/main/java/com/example/android/quizapp/QuizIntro.java
@@ -4,15 +4,11 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 public class QuizIntro extends Fragment {
-
     public QuizIntro() {
         // Required empty public constructor
     }
@@ -20,7 +16,7 @@ public class QuizIntro extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_quiz_intro, container, false);
-        return view;
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_quiz_intro, container, false);
     }
 }
--- app/src/main/java/com/example/android/quizapp/WomenHistoryQuiz.java
+++ app/src/main/java/com/example/android/quizapp/WomenHistoryQuiz.java
@@ -1,5 +1,6 @@
 package com.example.android.quizapp;
 
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -8,27 +9,49 @@ import android.os.Bundle;
 import android.view.View.OnClickListener;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class WomenHistoryQuiz extends AppCompatActivity {
+    int baseScore;
+    int points;
+    String isCorrect;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_women_history_quiz);
 
+        //Add Quiz Intro Fragment to Activity Layout
+        if (findViewById(R.id.output) != null) {
+            if (savedInstanceState != null) {
+                return;
+            }
+            QuizIntro firstFragment = new QuizIntro();
+            firstFragment.setArguments(getIntent().getExtras());
+            getSupportFragmentManager().beginTransaction().add(R.id.output, firstFragment).commit();
+        }
+    }
 
-        OnClickListener goToQuestions = new View.OnClickListener(){
-            public void onClick(View view) {
-                Fragment fragment = null;
-                if(view == findViewById(R.id.go_to_questions)){
-                    fragment = new Questions();
-                }
-                FragmentManager manager = getSupportFragmentManager();
-                FragmentTransaction transaction = manager.beginTransaction();
-                transaction.replace(R.id.output, fragment);
+    public void displayQuestions(View view){
+        //Switch to Question Fragment
+        Button startQuiz = findViewById(R.id.start_quiz_button);
+        startQuiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create fragment and give it an argument specifying the article it should show
+                Questions newFragment = new Questions();
+                Bundle args = new Bundle();
+                newFragment.setArguments(args);
+
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                // Replace whatever is in the fragment_container view with this fragment,
+                // and add the transaction to the back stack so the user can navigate back
+                transaction.replace(R.id.output, newFragment);
+                //transaction.addToBackStack(null);
+                // Commit the transaction
                 transaction.commit();
             }
-        };
-        Button theQuestionsButton = findViewById(R.id.go_to_questions);
-        theQuestionsButton.setOnClickListener(goToQuestions);
+        });
     }
 }
